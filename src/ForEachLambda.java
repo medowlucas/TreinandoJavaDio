426a197cf10c20bf214196e526ba3c4e38dfc48c
medowlucas <medowlucas@gmail.com>
2021-06-09 21:07:01 -0300

    Tentando enviar o commit para o github

---
+++ src/ForEachLambda.java
@@ -0,0 +1,15 @@
+import java.util.stream.IntStream;
+public class ForEachLambda {
+
+    public static void main(String[] args) {
+
+        IntStream.of(1,2,3,4,5).forEach(n-> {
+            System.out.println(n);
+        });
+
+        IntStream.range(0,3).forEach(n-> {
+            System.out.println("numero= "+n);
+        });
+
+    }
+}
